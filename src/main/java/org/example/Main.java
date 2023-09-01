57995f870b8aceadb45322107c11394f4c2aeb2a
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2023-09-01 16:51:19 +0900

    Create Project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args){
+        Student s1 = new Student("22000712", "조동운");
+        System.out.println("Hello World!");
+        System.out.println(s1.toString());
+    }
+}
